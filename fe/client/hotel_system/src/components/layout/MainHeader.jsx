c244eb8c845d5808309549b963f4997ff39fceba
NNHieu2003 <hieuhieu24403@gmail.com>
2024-11-07 19:18:34 +0700

    hotel

---
+++ fe/client/hotel_system/src/components/layout/MainHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const MainHeader = () => {
+	return (
+		<header className="header-banner">
+			<div className="overlay"></div>
+			<div className="animated-texts overlay-content">
+				<h1>
+					Welcome to Hotel
+				</h1>
+			</div>
+		</header>
+	)
+}
+
+export default MainHeader
